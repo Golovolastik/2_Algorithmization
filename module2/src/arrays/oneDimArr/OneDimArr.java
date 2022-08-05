32014208ad1388b286a74881cd27bfff58f31ac5
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-05 12:41:27 +0300

    add methods for double

--- module2/src/arrays/oneDimArr/OneDimArr.java
+++ module2/src/arrays/oneDimArr/OneDimArr.java
@@ -3,13 +3,25 @@ package arrays.oneDimArr;
 import java.util.Random;
 
 public class OneDimArr {
-    static void create(int[] arr, int size){
+    static void initInt(int[] arr, int size){
         Random random = new Random();
         for (int i=0; i<size; i++){
             arr[i] = random.nextInt() % 100;
         }
     }
-    static void print(int[] arr, int size){
+    static void initDouble(double[] arr, int size){
+        Random random = new Random();
+        for (int i=0; i<size; i++){
+            arr[i] = (-0.5 + random.nextDouble()) * 10;
+        }
+    }
+    static void printInt(int[] arr, int size){
+        for (int i=0; i<size; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("\n");
+    }
+    static void printDouble(double[] arr, int size){
         for (int i=0; i<size; i++){
             System.out.print(arr[i] + " ");
         }
