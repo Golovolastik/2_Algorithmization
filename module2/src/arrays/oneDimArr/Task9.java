eb84465dcd858cf8f1fedab564511f2b5128a9f0
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-06 19:15:18 +0300

    Begin task 9

---
+++ module2/src/arrays/oneDimArr/Task9.java
@@ -0,0 +1,12 @@
+package arrays.oneDimArr;
+
+// В массиве целых чисел размером N найти наиболее часто встречающееся.
+// Если таких чисел несколько, то определить наименьшее из них
+public class Task9 {
+    public static void main(String[] args){
+        final int SIZE = 10;
+        int[] array = new int[SIZE];
+
+    }
+}
+
