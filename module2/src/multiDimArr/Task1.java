67ae70635beb1e14c60665e331d8a823ad4377ab
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-07 12:40:22 +0300

    Begin to do

--- module2/src/multiDimArr/Task1.java
+++ module2/src/multiDimArr/Task1.java
@@ -1,4 +1,13 @@
 package multiDimArr;
 
+// Дана матрица.
+// Вывести на экран все нечетные столбцы,
+// у которых первый элемент больше последнего.
 public class Task1 {
+    public static void main(String[] args){
+        final int SIZE = 5;
+        int[][] array = new int[SIZE][SIZE];
+        MultiDimArr.initTwoDInt(array, SIZE, SIZE);
+        MultiDimArr.printTwoDInt(array, SIZE, SIZE);
+    }
 }
