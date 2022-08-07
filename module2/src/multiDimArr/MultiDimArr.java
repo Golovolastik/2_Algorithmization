83c1cea162f82a9e51478b33f7600c62c55cc579
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-08 01:52:39 +0300

    methods were modified

--- module2/src/multiDimArr/MultiDimArr.java
+++ module2/src/multiDimArr/MultiDimArr.java
@@ -3,17 +3,16 @@ package multiDimArr;
 import java.util.Random;
 
 public class MultiDimArr {
-    static void initTwoDInt(int arr[][], int columnNumber, int rowNumber){
+    static void initTwoDInt(int[][] arr, int rowNumber, int columnNumber){
         Random random = new Random();
         for (int i=0; i<rowNumber; i++){
             for (int j=0; j<columnNumber; j++){
-                arr[i][j] = random.nextInt() % 100;
+                arr[i][j] = random.nextInt(1, 20);
             }
         }
     }
 
-    static void printTwoDInt(int arr[][], int columnNumber, int rowNumber){
-        Random random = new Random();
+    static void printTwoDInt(int[][] arr, int rowNumber, int columnNumber){
         for (int i=0; i<rowNumber; i++){
             for (int j=0; j<columnNumber; j++){
                 System.out.printf("%4d ", arr[i][j]);
