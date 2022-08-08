93b425592d12748d4a503b72c1b7602b840e35b2
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-08 12:17:23 +0300

    Task 3 complete

--- module2/src/multiDimArr/Task3.java
+++ module2/src/multiDimArr/Task3.java
@@ -5,8 +5,21 @@ public class Task3 {
     public static void main(String[] args){
         final int ROW = 4;
         final int COLUMN = 6;
+        int k = 2;
+        int p = 4;
         int[][] array = new int[ROW][COLUMN];
         MultiDimArr.initTwoDInt(array, ROW, COLUMN);
         MultiDimArr.printTwoDInt(array, ROW, COLUMN);
+        System.out.println();
+        for (int i=0; i<ROW; i++){
+            for (int j=0; j<COLUMN; j++){
+                if (j == p-1 || i == k-1){
+                    System.out.printf("%4d ", array[i][j]);
+                } else {
+                    System.out.printf("     ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
